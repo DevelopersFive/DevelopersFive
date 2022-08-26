1093377d6858672e2ab89a5f32fcb44951bed78e
molinaciclo3 <molinamath50@gmail.com>
2022-08-25 19:08:45 -0500

    feat: iniciando en localhost8080

---
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/DevelopersFiveApplication.java
@@ -0,0 +1,20 @@
+package com.co.udea.mintic.developersFive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DevelopersFiveApplication {
+	@RequestMapping("/")
+	String landingPage(){
+		return "BIENVENIDOS";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopersFiveApplication.class, args);
+	}
+
+}
