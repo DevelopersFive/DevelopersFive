13fbade01356727448938dc9757dc62b222b1484
Andramao <111099632+Andramao@users.noreply.github.com>
2022-08-27 22:43:58 -0500

    feat:se modelan clases empresa, empleado, y movimiento de dinero

--- developersFive/src/main/java/com/co/udea/mintic/developersFive/domain/Empresa.java
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/domain/Empresa.java
@@ -1,4 +1,19 @@
 package com.co.udea.mintic.developersFive.domain;
 
+import lombok.Data;
+
+@Data
 public class Empresa {
+
+    private String nombre;
+    private String documento;
+    private String telefono;
+    private String direccion;
+    private String nit;
+    private int id;
+    //private ArraysLIst<Empleado> empleado;
+    //private ArrayList<Transaccion>transaccion;
+
+
+
 }
