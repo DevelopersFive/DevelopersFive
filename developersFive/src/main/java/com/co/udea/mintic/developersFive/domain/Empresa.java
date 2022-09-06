dcc4892bfc3f0ee1e4e86c98893249241daf9e5c
Andramao <111099632+Andramao@users.noreply.github.com>
2022-09-05 22:46:53 -0500

    feat: se sube controlador y servivio de la clase empresa provicional para revisar. Andramao

--- developersFive/src/main/java/com/co/udea/mintic/developersFive/domain/Empresa.java
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/domain/Empresa.java
@@ -1,19 +1,32 @@
 package com.co.udea.mintic.developersFive.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Setter
+@Getter
 public class Empresa {
 
     private String nombre;
-    private String documento;
     private String telefono;
     private String direccion;
     private String nit;
     private int id;
-    //private ArraysLIst<Empleado> empleado;
+    //private ArrayLIst<Empresa> empresa;
     //private ArrayList<Transaccion>transaccion;
 
 
+    public Empresa(String nombre, String telefono, String direccion, String nit, int id) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.nit = nit;
+        this.id = id;
+    }
 
+    public Empresa() {
+
+    }
 }
