39c7d3fd70a6ef63f9c5db79ff44108ff3e4f31f
giovanoty <giovanoty9@hotmail.com>
2022-09-13 23:34:59 -0500

    Modificación de los Path

--- developersFive/src/main/java/com/co/udea/mintic/developersFive/controller/ControllerMovimientoDinero.java
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/controller/ControllerMovimientoDinero.java
@@ -1,7 +1,10 @@
 package com.co.udea.mintic.developersFive.controller;
 
+import com.co.udea.mintic.developersFive.domain.Empleado;
 import com.co.udea.mintic.developersFive.domain.MovimientoDinero;
 import com.co.udea.mintic.developersFive.services.ServiceMovimientoDinero;
+import com.co.udea.mintic.developersFive.services.ServiceUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,28 +12,25 @@ import java.util.List;
 @RestController
 public class ControllerMovimientoDinero {
 
+    @Autowired
     ServiceMovimientoDinero sermovd1;
 
-    public ControllerMovimientoDinero(ServiceMovimientoDinero sermovd1) {
-        this.sermovd1 = sermovd1;
-    }
-
-    @GetMapping("/movements")
+    @GetMapping(/*"/movements"*/path = "/udea/mintic/movements", produces = "application/json")
     public List<MovimientoDinero> informacion() {
         return this.sermovd1.getInformacion();
     }
 
-    @PostMapping("/movements")
+    @PostMapping(/*"/movements"*/path = "/udea/mintic/movements", produces = "application/json")
     public MovimientoDinero crearEmpleado(@RequestBody MovimientoDinero movdin) {
         return this.sermovd1.crearMovimientoDinero(movdin);
     }
 
-    @PutMapping("/movements/{id}")
+    @PutMapping(/*"/movements/{id}"*/path = "/udea/mintic/movements/{id}", produces = "application/json")
     public MovimientoDinero actualizarEmpresa(@PathVariable Long id, @RequestBody MovimientoDinero movdin) {
         return this.sermovd1.actualizarMovimientoDinero(id, movdin);
     }
 
-    @DeleteMapping("/movements/{id}")
+    @DeleteMapping(/*"/movements/{id}"*/path = "/udea/mintic/movements/{id}", produces = "application/json")
     public MovimientoDinero eliminarEmpresa(@PathVariable(value = "id") Long id) {
         return this.sermovd1.eliminarMovimientoDinero(id);
     }
