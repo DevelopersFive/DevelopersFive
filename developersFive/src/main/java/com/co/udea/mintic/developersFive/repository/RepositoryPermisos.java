a1ec2d379362fa28deea300ec6f3ea88940f6ddd
Andramao <111099632+Andramao@users.noreply.github.com>
2022-09-19 17:52:03 -0500

    feat: Se realizan cambios en controllerEmpresa serviceEmpresa, se crean repositoryEmpresa, entityEmpresa, entityPermisos y repositoryPermisos. se prueba y permite cambios en supabase. Andramao

--- developersFive/src/main/java/com/co/udea/mintic/developersFive/repository/RepositoryPermisos.java
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/repository/RepositoryPermisos.java
@@ -1,10 +1,10 @@
 package com.co.udea.mintic.developersFive.repository;
 
-import com.co.udea.mintic.developersFive.domain.MovimientoDinero;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepositoryMovimientoDinero extends JpaRepository<MovimientoDinero, Long> {
-}
+    public interface RepositoryPermisos extends JpaRepository<EntityPermisos, Long> {
 
+}
