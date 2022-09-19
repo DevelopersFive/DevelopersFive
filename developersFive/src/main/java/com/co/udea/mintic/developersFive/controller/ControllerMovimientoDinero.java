de5c340a80f6b80b34f2b3680a040db7316eba59
giovanoty <giovanoty9@hotmail.com>
2022-09-18 22:44:16 -0500

    Se vuelven a dejar los path anteriores

--- developersFive/src/main/java/com/co/udea/mintic/developersFive/controller/ControllerMovimientoDinero.java
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/controller/ControllerMovimientoDinero.java
@@ -15,22 +15,26 @@ public class ControllerMovimientoDinero {
     @Autowired
     ServiceMovimientoDinero sermovd1;
 
-    @GetMapping(/*"/movements"*/path = "/udea/mintic/movements", produces = "application/json")
+    public ControllerMovimientoDinero(ServiceMovimientoDinero sermovd1) {
+        this.sermovd1 = sermovd1;
+    }
+
+    @GetMapping("/movements")
     public List<MovimientoDinero> informacion() {
         return this.sermovd1.getInformacion();
     }
 
-    @PostMapping(/*"/movements"*/path = "/udea/mintic/movements", produces = "application/json")
+    @PostMapping("/movements")
     public MovimientoDinero crearEmpleado(@RequestBody MovimientoDinero movdin) {
         return this.sermovd1.crearMovimientoDinero(movdin);
     }
 
-    @PutMapping(/*"/movements/{id}"*/path = "/udea/mintic/movements/{id}", produces = "application/json")
+    @PutMapping("/movements/{id}")
     public MovimientoDinero actualizarEmpresa(@PathVariable Long id, @RequestBody MovimientoDinero movdin) {
         return this.sermovd1.actualizarMovimientoDinero(id, movdin);
     }
 
-    @DeleteMapping(/*"/movements/{id}"*/path = "/udea/mintic/movements/{id}", produces = "application/json")
+    @DeleteMapping("/movements/{id}")
     public MovimientoDinero eliminarEmpresa(@PathVariable(value = "id") Long id) {
         return this.sermovd1.eliminarMovimientoDinero(id);
     }
