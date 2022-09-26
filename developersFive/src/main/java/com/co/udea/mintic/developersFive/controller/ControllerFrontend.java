d0181b86b70919e127f078c5de925725ce97ec76
fabiandres14 <fabianbm147@gmail.com>
2022-09-25 20:45:25 -0500

    se empieza modificaciones en controllerfronted y creacion de templetes

--- developersFive/src/main/java/com/co/udea/mintic/developersFive/controller/ControllerFrontend.java
+++ developersFive/src/main/java/com/co/udea/mintic/developersFive/controller/ControllerFrontend.java
@@ -1,2 +1,54 @@
-package com.co.udea.mintic.developersFive.controller;public class ControllerFrontend {
+package com.co.udea.mintic.developersFive.controller;
+
+import com.co.udea.mintic.developersFive.services.ServiceUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ControllerFrontend {
+
+    @Autowired
+    ServiceUsuario serviceUsuario;
+
+    @GetMapping (path = "/")
+    public String home(){
+
+        return "index";
+    }
+
+    @GetMapping (path = "/dashboard")
+    public String dashboard(){
+
+
+
+            return "dashboard";
+        }
+
+
+
+    @GetMapping (path = "/pagina2prueba")
+    public String pagina2prueba(){
+
+
+
+        return "index";
+    }
+
+    @GetMapping (path = "/crearUsuario")
+    public String crearUsuario (){
+
+
+
+        return "crearUsuario";
+
+    }
+
 }
+
+
+
+
+
